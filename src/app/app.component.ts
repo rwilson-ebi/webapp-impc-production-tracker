afb4aa5f770389a045701320a20d7cc2e21fb7cf
rwilson-ebi <rwilson@ebi.ac.uk>
2019-09-19 16:53:15 +0100

    Changed the reference to the environment file in app.components.ts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { SelectItem } from 'primeng/api';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {environment} from '../environments/environment.prod';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
